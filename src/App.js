3d56079a2280fc4c44c9f2f8bb456cee8774f1a8
estudiante <andreuz33@gmail.com>
2019-09-13 17:23:02 -0500

    commit4

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1>{props.parsedData}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/source/parser/index.js
@@ -0,0 +1,9 @@
+function Parser(){
+    Parser.prototype.parseData = function (inputData){
+        return{
+            data:"something here"
+        }
+    }
+}
+
+export default Parser;
